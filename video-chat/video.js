226ca6daeda3bf1b8453de692dcf697fb36ce951
Hamza525318 <hamzamustafa273@gmail.com>
2024-05-03 19:34:15 +0530

    setting up frontend for video display

--- video-chat/video.js
+++ video-chat/video.js
@@ -1,9 +1,10 @@
 const currentUrl = window.location.href;
 const urlParams = new URLSearchParams(currentUrl);
 const meeting_id = urlParams.toString().substring(55);
+const meeting_id_header = document.getElementById("meeting_id_tag")
 
 //console.log(meeting_id);
-
+meeting_id_header.innerText = meeting_id
 const socket = io.connect("https://localhost:8000",{
     auth:{
         meeting_id,
